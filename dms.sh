6f20d80b4e884b19885b8b28944d5e9ad6ef3930
Huggable Hacker <huggablehacker@users.noreply.github.com>
2017-02-15 15:24:01 -0500

    Update dms.sh

--- dms.sh
+++ dms.sh
@@ -1,4 +1,4 @@
-# wget is generally available on almost any 'nix machine and can produce the mirror from the command line. However, wget seems to have problems converting the relative style sheet URLs properly with many Drupal site pages.
+# wget is generally available on almost any 'nix machine and can produce the mirror from the command line. However, wget seems to have problems converting the relative style sheet URLs properly with many site pages.
 
 # wget respects the robots.txt files, so might not download some of the files in /sites/ or elsewhere. To disable this, include the option -e robots=off in your command line.
 
